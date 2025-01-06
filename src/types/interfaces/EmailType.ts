99c26290d3e6ecdb44fdd2d430e76f4315f95b6a
ciringa <thierrird957468@gmail.com>
2025-01-06 11:56:34 -0300

    send validation email

--- src/types/interfaces/EmailType.ts
+++ src/types/interfaces/EmailType.ts
@@ -1,3 +1,9 @@
+/** Interface de email utilizada para enviar emails
+ * @argument text - Corpo do email(texto principal a ser enviado)
+ * @argument subject - Cabeçalho / titulo do email
+ * @argument to - Email que irá receber a mensagem (alvo do email)
+*/
+
 export interface EmailType{
     text: string
     to: string 
